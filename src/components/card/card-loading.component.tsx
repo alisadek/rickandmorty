40c746cfb25419e40f689d60c31229fa96bb12d4
alisadekk <alysadekk@gmail.com>
2022-12-23 15:37:52 +0200

    fix: increase card height to take correct space

--- src/components/card/card-loading.component.tsx
+++ src/components/card/card-loading.component.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const LoadingCard = (props: Props) => {
   return (
-    <Grid xl={1.7} lg={3} md={4} sm={6} xs={12} item>
+    <Grid xl={1.7} lg={3} md={4} sm={6} xs={12} height="100%" item>
       <Skeleton variant="rectangular" animation="pulse" height="60%" />
 
       <Box sx={{ pt: 0.5 }}>
