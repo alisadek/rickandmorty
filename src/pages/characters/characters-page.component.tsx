58978679fb489ab6e476e8e3f74a7994488bb759
alisadekk <alysadekk@gmail.com>
2022-12-23 15:38:11 +0200

    feature: implement search feature on characters

--- src/pages/characters/characters-page.component.tsx
+++ src/pages/characters/characters-page.component.tsx
@@ -1,56 +1,78 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Grid from "@mui/material/Grid";
 import { GET_ALL_CHARACTERS } from "../../queries.graphql";
 import { Characters as CharactersType } from "../../__generated__/graphql";
-import MediaCard from "../../components/card/card.component";
 import { useQuery } from "@apollo/client";
-import { Pagination, Stack } from "@mui/material";
-import LoadingCard from "../../components/card/card-loading.component";
+import { Pagination } from "@mui/material";
+import SideNav from "../../components/side-nav/side-nav.component";
+import { debounce } from "lodash";
+import CharactersCollection from "./characters-collection/characters-collection.component";
+import Stack from "@mui/material/Stack";
 
 type Props = {};
 
 const Characters = (props: Props) => {
   const [selectedPage, setSelectedPage] = useState(1);
+  const [searchValue, setSearchValue] = useState<string | undefined>(undefined);
   const { data, loading, error } = useQuery<{ characters: CharactersType }>(
     GET_ALL_CHARACTERS,
-    { variables: { page: selectedPage } }
+    {
+      variables: { page: selectedPage, name: searchValue },
+    }
   );
   const handleChange = (e: React.ChangeEvent<unknown>, value: number) =>
     setSelectedPage(value);
 
-  const LOADING_LIST = Array(20).fill(0, 0);
-  console.log(LOADING_LIST);
   if (error) <p style={{ color: "#fff" }}>{`${error?.message}`}</p>;
+  const handleSearchChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: string
+  ) => {
+    setSearchValue(value);
+  };
+  const searchDelayed = debounce(handleSearchChange, 200);
+  const options = useMemo(
+    () =>
+      data?.characters?.results?.map((char) => ({
+        label: char?.name as string,
+        value: {
+          id: char?.id,
+          name: char?.name,
+          image: char?.image,
+          status: char?.status,
+          species: char?.species,
+        },
+      })),
+    [data]
+  );
 
   return (
-    <Stack sx={{ height: "92vh" }} justifyContent="space-between" paddingX={5}>
-      <Grid
-        sx={{ color: "#fff", height: "90vh", overflowY: "" }}
-        container
-        spacing={3}
-        paddingTop={5}
-      >
-        {loading
-          ? LOADING_LIST.map(() => <LoadingCard />)
-          : data?.characters?.results?.map((char) => (
-              <MediaCard
-                key={char?.id}
-                image={char?.image}
-                name={char?.name}
-                species={char?.species}
-                status={char?.status}
-              />
-            ))}
+    <Grid container xs={12} height="95vh">
+      <Grid container item xs={2}>
+        <SideNav
+          loading={loading}
+          placeholder="Search Characters"
+          onSearchInputChange={searchDelayed}
+          inputValue={searchValue}
+          options={options}
+        />
       </Grid>
-
-      <Pagination
-        sx={{ alignSelf: "flex-end" }}
-        variant="outlined"
-        count={42}
-        onChange={handleChange}
-        page={selectedPage}
-      />
-    </Stack>
+      <Grid container item xs={10} paddingX={5}>
+        <Stack height="100%" width="100%">
+          <CharactersCollection
+            characters={data?.characters?.results}
+            loading={loading}
+          />
+          <Pagination
+            sx={{ position: "absolute", bottom: "20px", right: "20px" }}
+            variant="outlined"
+            count={data?.characters.info?.pages as number}
+            onChange={handleChange}
+            page={selectedPage}
+          />
+        </Stack>
+      </Grid>
+    </Grid>
   );
 };
 
