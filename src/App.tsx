f8cc284170c4400fec99e8a018788740a4ba9799
Ali Sadek <alysadekk@gmail.com>
2022-12-25 11:45:10 +0200

    optimize: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import NavBar from "./components/navbar/navbar.component";
-import Home from "./pages/home/home-page.component";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Characters from "./pages/characters/characters-page.component";
-import Episodes from "./pages/episodes/episodes-page.component";
-import Locations from "./pages/locations/locations-page.component";
-import { createTheme, ThemeProvider } from "@mui/material";
-import CharacterWiki from "./pages/character-wiki/character-wiki.component";
+import { CircularProgress, createTheme, ThemeProvider } from "@mui/material";
+const Home = lazy(() => import("./pages/home/home-page.component"));
+const NavBar = lazy(() => import("./components/navbar/navbar.component"));
+const Characters = lazy(
+  () => import("./pages/characters/characters-page.component")
+);
+const Episodes = lazy(() => import("./pages/episodes/episodes-page.component"));
+const Locations = lazy(
+  () => import("./pages/locations/locations-page.component")
+);
+const CharacterWiki = lazy(
+  () => import("./pages/character-wiki/character-wiki.component")
+);
 
 function App() {
   const darkTheme = createTheme({
@@ -15,17 +21,19 @@ function App() {
     },
   });
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Routes>
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<Home />} />
-          <Route path="characters" element={<Characters />} />
-          <Route path="character/:id" element={<CharacterWiki />} />
-          <Route path="episodes" element={<Episodes />} />
-          <Route path="locations" element={<Locations />} />
-        </Route>
-      </Routes>
-    </ThemeProvider>
+    <Suspense fallback={<CircularProgress />}>
+      <ThemeProvider theme={darkTheme}>
+        <Routes>
+          <Route path="/" element={<NavBar />}>
+            <Route index element={<Home />} />
+            <Route path="characters" element={<Characters />} />
+            <Route path="character/:id" element={<CharacterWiki />} />
+            <Route path="episodes" element={<Episodes />} />
+            <Route path="locations" element={<Locations />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
